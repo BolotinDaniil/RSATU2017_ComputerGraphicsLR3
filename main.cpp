5a2758b9829b014f5a7b53a2d9eb2672fd3c5cef
BolotinDaniil <bolotin_daniil522@mail.ru>
2017-01-12 12:23:50 +0300

    not ready

--- main.cpp
+++ main.cpp
@@ -451,6 +451,87 @@ void ReSizeGLScene(GLsizei width, GLsizei height){
     winHeight = height;
 }
 
+/*
+   Determine whether or not the line segment p1,p2
+   Intersects the 3 vertex facet bounded by pa,pb,pc
+   Return true/false
+
+
+   The equation of the line is p = p1 + mu (p2 - p1)
+   The equation of the plane is a x + b y + c z + d = 0
+                                n.x x + n.y y + n.z z + d = 0
+*/
+
+bool lineFacet(vec3 &p1, vec3 &p2, const Face &face)
+{
+   double EPS = (double)0.00000000001;
+   double d;
+   double a1,a2,a3;
+   double total,denom,mu;
+   vec3 n, pa1, pa2, pa3, p;
+	
+   vec3 pa = face.points[0].v;
+   vec3 pb = face.points[1].v;
+   vec3 pc = face.points[2].v;
+
+
+   /* Calculate the parameters for the plane */
+   n.x = (pb.y - pa.y)*(pc.z - pa.z) - (pb.z - pa.z)*(pc.y - pa.y);
+   n.y = (pb.z - pa.z)*(pc.x - pa.x) - (pb.x - pa.x)*(pc.z - pa.z);
+   n.z = (pb.x - pa.x)*(pc.y - pa.y) - (pb.y - pa.y)*(pc.x - pa.x);
+   n = Normalized(n);
+   d = - n.x * pa.x - n.y * pa.y - n.z * pa.z;
+
+
+   /* Calculate the position on the line that intersects the plane */
+   denom = n.x * (p2.x - p1.x) + n.y * (p2.y - p1.y) + n.z * (p2.z - p1.z);
+   if (ABS(denom) < EPS)         /* Line and plane don't intersect */
+      return(FALSE);
+   mu = - (d + n.x * p1.x + n.y * p1.y + n.z * p1.z) / denom;
+   p.x = p1.x + mu * (p2.x - p1.x);
+   p.y = p1.y + mu * (p2.y - p1.y);
+   p.z = p1.z + mu * (p2.z - p1.z);
+   if (mu < 0 || mu > 1)   /* Intersection not along line segment */
+      return(FALSE);
+
+
+   /* Determine whether or not the intersection point is bounded by pa,pb,pc */
+   pa1.x = pa.x - p.x;
+   pa1.y = pa.y - p.y;
+   pa1.z = pa.z - p.z;
+   pa1 = Normalized(pa1);
+   pa2.x = pb.x - px;
+   pa2.y = pb.y - py;
+   pa2.z = pb.z - pz;
+   pa2 = Normalized(pa2);
+   pa3.x = pc.x - px;
+   pa3.y = pc.y - py;
+   pa3.z = pc.z - pz;
+   pa3 = Normalized(pa3);
+   a1 = pa1.x*pa2.x + pa1.y*pa2.y + pa1.z*pa2.z;
+   a2 = pa2.x*pa3.x + pa2.y*pa3.y + pa2.z*pa3.z;
+   a3 = pa3.x*pa1.x + pa3.y*pa1.y + pa3.z*pa1.z;
+   total = (acos(a1) + acos(a2) + acos(a3)) * RTOD;
+   if (ABS(total - 360) > EPS)
+      return(FALSE);
+
+   return(TRUE);
+}
+
+bool edgeVisible(const Edge &edge, const Model &model, const vec3 &eye){
+	bool visible = true;
+	for (int i=0; i<model.faces.size(); i++){
+		p = edge.pointA;
+		if (lineFacet(p, eye, model.faces[i]))
+			return false;
+
+		p = edge.pointB;
+		if (lineFacet(p, eye, model.faces[i]))
+			return false;
+	}
+	return visible;
+}
+
 void drawContour(Model& model, const vec3& eye){
 	glLineWidth(3.0);
 	
@@ -480,7 +561,8 @@ void drawContour(Model& model, const vec3& eye){
             double b = nb & v0;
             // double angle = GetAngle(na, nb);
 
-            if (a*b <= 0) {
+            if (a*b <= 0) 
+			if (edgeVisible(model.edges[i], model, eye)){
 				edge->wasVisible = true;
                 Point p;
                 glBegin(GL_LINES);
@@ -518,7 +600,7 @@ void renderModel(GLint vpX, GLint vpY, GLsizei vpWidth, GLsizei vpHeight, Model&
 	glColor3f(model.color.x, model.color.y, model.color.z);
 	
     glPushMatrix();
-    //if (!model.drawContour) {
+    if (!model.drawContour) {
         for (int i = 0; i < model.faces.size(); i++) {
             glBegin(GL_TRIANGLES);
             for (int j = 0; j < 3; j++) {
@@ -529,7 +611,7 @@ void renderModel(GLint vpX, GLint vpY, GLsizei vpWidth, GLsizei vpHeight, Model&
             }
             glEnd();
         }
-    //}
+    }
     glPopMatrix();
 	
 	if (model.drawContour){
